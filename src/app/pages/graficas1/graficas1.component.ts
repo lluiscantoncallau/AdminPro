78256045bead55fcf7d0590c077cd262719cc2f7
LLUIS CANTON CALLAU <lluis.cantoncallau@altran.com>
2020-04-13 20:51:57 +0200

    tslint recomendations

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -8,31 +8,31 @@ import { ChartType } from 'chart.js';
   styles: []
 })
 export class Graficas1Component implements OnInit {
- 
+
   graficos: any = {
-    'grafico1': {
-      'labels': ['Con Frijoles', 'Con Natilla', 'Con tocino'],
-      'data': [24, 30, 46],
-      'type': 'doughnut',
-      'leyenda': 'El pan se come con'
+    grafico1: {
+      labels: ['Con Frijoles', 'Con Natilla', 'Con tocino'],
+      data: [24, 30, 46],
+      type: 'doughnut',
+      leyenda: 'El pan se come con'
     },
-    'grafico2': {
-      'labels': ['Hombres', 'Mujeres'],      
-      'data': [4500, 6000],
-      'type': 'doughnut',
-      'leyenda': 'Entrevistados'
+    grafico2: {
+      labels: ['Hombres', 'Mujeres'],
+      data: [4500, 6000],
+      type: 'doughnut',
+      leyenda: 'Entrevistados'
     },
-    'grafico3': {
-      'labels': ['Si', 'No'],
-      'data': [95, 5],
-      'type': 'doughnut',
-      'leyenda': '¿Le dan gases los frijoles?'
+    grafico3: {
+      labels: ['Si', 'No'],
+      data: [95, 5],
+      type: 'doughnut',
+      leyenda: '¿Le dan gases los frijoles?'
     },
-    'grafico4': {
-      'labels': ['No', 'Si'],
-      'data': [85, 15],
-      'type': 'doughnut',
-      'leyenda': '¿Le importa que le den gases?'
+    grafico4: {
+      labels: ['No', 'Si'],
+      data: [85, 15],
+      type: 'doughnut',
+      leyenda: '¿Le importa que le den gases?'
     },
   };
 
